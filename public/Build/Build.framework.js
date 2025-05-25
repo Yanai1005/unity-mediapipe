b2e26866300e0d40f81a476ab3780d3e75984e1e
柳井 <108969330+Yanai1005@users.noreply.github.com>
2025-05-25 12:46:41 +0900

    wip 当たり判定

--- public/Build/Build.framework.js
+++ public/Build/Build.framework.js
@@ -1994,13 +1994,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  3679120: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 3679181: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 3679245: function() {return Module.webglContextAttributes.powerPreference;},  
- 3679303: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 3679358: function($0) {performance.now = function() { return $0; };},  
- 3679406: function($0) {performance.now = function() { return $0; };},  
- 3679454: function() {performance.now = Module['emscripten_get_now_backup'];}
+  3680304: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 3680365: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 3680429: function() {return Module.webglContextAttributes.powerPreference;},  
+ 3680487: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 3680542: function($0) {performance.now = function() { return $0; };},  
+ 3680590: function($0) {performance.now = function() { return $0; };},  
+ 3680638: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
